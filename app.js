cd4eff7bd230a99ce4eea133267e67774f637e97
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-01-27 10:13:49 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// AJAX call to get messages
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'data.json', true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var data = JSON.parse(xhr.responseText);
+    var messages = document.getElementById("messages");
+    // loop through messages and add them to the page
+    for (var i = 0; i < data.length; i++) {
+      var entry = document.createElement("div");
+      var paragraph = document.createElement("p");
+      var name = document.createElement("strong");
+      name.textContent = data[i].name;
+      paragraph.appendChild(name);
+      paragraph.appendChild(document.createTextNode(": " + data[i].message));
+      entry.appendChild(paragraph);
+
+      // add reply div if reply is not empty
+      if(data[i].rname != "" || data[i].rmessage != "") {
+        var reply = document.createElement("div");
+        var reply_name = document.createElement("strong");
+        reply_name.textContent = data[i].rname;
+        var reply_message = document.createElement("p");
+        reply_message.appendChild(reply_name);
+        reply_message.appendChild(document.createTextNode(": " + data[i].rmessage));
+        reply.appendChild(reply_message);
+        entry.appendChild(reply);
+      }
+      messages.appendChild(entry);
+    }
+  }
+};
+xhr.send();
---
+++ submit.php
@@ -0,0 +1,37 @@
+<?php
+// check if form was submitted
+if(isset($_POST['submit'])){
+    // get form data
+    $name = $_POST['name'];
+    $message = $_POST['message'];
+    // set rname and rmessage to empty string
+    $rname = "";
+    $rmessage = "";
+
+    // if name is empty, set it to "anon"
+    if(empty($name)){
+        $name = "anon";
+    }
+
+    // if message is not empty
+    if(!empty($message)){
+        // get existing json data
+        $data = json_decode(file_get_contents('data.json'), true);
+        $flag = true;
+        // loop through previous submissions
+        foreach ($data as $submission) {
+            // check if current submission matches any previous submissions in both name and message
+            if ($submission['name'] == $name && $submission['message'] == $message) {
+                $flag = false;
+                break;
+            }
+        }
+        // if current submission does not match any previous submissions, append it to the JSON file
+        if ($flag) {
+            $data[] = array("name" => $name, "message" => $message,"rname" => $rname,"rmessage" => $rmessage);
+            file_put_contents('data.json', json_encode($data));
+        }
+    }
+}
+header("Location: https://articexploit.xyz:8443/test/");
+exit();
