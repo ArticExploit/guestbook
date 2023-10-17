405161d3017c5e719e78b80df7f39c394df1097e
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-10-17 12:27:02 +0000

    php print system

---
+++ print.php
@@ -0,0 +1,20 @@
+<?php
+// Fetch and decode JSON data
+$json = file_get_contents('data.json');
+$data = json_decode($json, true);
+
+// Loop through data and generate HTML
+foreach ($data as $item) {
+    echo '<div class="entrysq">';
+    echo '<p><strong>' . htmlspecialchars($item['name']) . '</strong>: ' . htmlspecialchars($item['message']) . '</p>';
+
+    // Add reply div if reply is not empty
+    if (!empty($item['rname']) || !empty($item['rmessage'])) {
+        echo '<div class="entrysq">';
+        echo '<p><strong>' . htmlspecialchars($item['rname']) . '</strong>: ' . htmlspecialchars($item['rmessage']) . '</p>';
+        echo '</div>';
+    }
+
+    echo '</div>';
+}
+?>
