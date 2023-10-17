dd0ab1bec79c733c7bcfee2afc3254dedb982217
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-10-17 12:29:45 +0000

    changed the div names

--- print.php
+++ print.php
@@ -5,12 +5,12 @@ $data = json_decode($json, true);
 
 // Loop through data and generate HTML
 foreach ($data as $item) {
-    echo '<div class="entrysq">';
+    echo '<div class="comment">';
     echo '<p><strong>' . htmlspecialchars($item['name']) . '</strong>: ' . htmlspecialchars($item['message']) . '</p>';
 
     // Add reply div if reply is not empty
     if (!empty($item['rname']) || !empty($item['rmessage'])) {
-        echo '<div class="entrysq">';
+        echo '<div class="reply">';
         echo '<p><strong>' . htmlspecialchars($item['rname']) . '</strong>: ' . htmlspecialchars($item['rmessage']) . '</p>';
         echo '</div>';
     }
