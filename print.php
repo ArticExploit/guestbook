31abe098dcd531446cee84b82332c50db46dcad3
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-10-17 14:30:58 +0000

    added check for empty data file

--- print.php
+++ print.php
@@ -3,18 +3,21 @@
 $json = file_get_contents('data.json');
 $data = json_decode($json, true);
 
-// Loop through data and generate HTML
-foreach ($data as $item) {
-    echo '<div class="comment">';
-    echo '<p><strong>' . htmlspecialchars($item['name']) . '</strong>: ' . htmlspecialchars($item['message']) . '</p>';
+// Check if data is not empty
+if (!empty($data)) {
+    // Loop through data and generate HTML
+    foreach ($data as $item) {
+        echo '<div class="comment">';
+        echo '<p><strong>' . htmlspecialchars($item['name']) . '</strong>: ' . htmlspecialchars($item['message']) . '</p>';
 
-    // Add reply div if reply is not empty
-    if (!empty($item['rname']) || !empty($item['rmessage'])) {
-        echo '<div class="reply">';
-        echo '<p><strong>' . htmlspecialchars($item['rname']) . '</strong>: ' . htmlspecialchars($item['rmessage']) . '</p>';
+        // Add reply div if reply is not empty
+        if (!empty($item['rname']) || !empty($item['rmessage'])) {
+            echo '<div class="reply">';
+            echo '<p><strong>' . htmlspecialchars($item['rname']) . '</strong>: ' . htmlspecialchars($item['rmessage']) . '</p>';
+            echo '</div>';
+        }
+
         echo '</div>';
     }
-
-    echo '</div>';
 }
 ?>
