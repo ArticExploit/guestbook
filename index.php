fc9c500fee837956bc3a72ae34ac1f20a5837d36
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2023-10-24 03:30:58 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html>
 <body>
-    <div class="entry">
-        <h2>Leave a Message</h2>
-        <form action="submit.php" method="post">
-            <input type="text" id="name" name="name" placeholder="enter name or leave empty for anon"><br><br>
-            <textarea id="message" name="message" placeholder="enter your message"></textarea><br><br>
-            <input type="submit" name="submit" value="Submit">
-        </form>
-        <div>
-            <h2>Messages</h2>
-            <div id="messages">
-                <?php include 'print.php'; ?>
-            </div>
-        </div>
-    </div>
+    <?php include '/var/www/html/comments.php'; ?>
 </body>
 </html>
