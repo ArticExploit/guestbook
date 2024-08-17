e4c66475e42a271a1a3a20a6a7b549d3e4a0bb6b
ArticExploit <87346889+ArticExploit@users.noreply.github.com>
2024-08-17 21:46:47 +0000

    fixed :D

--- comments.php
+++ comments.php
@@ -1,38 +1,36 @@
 <?php
-// Initialize the character limit for messages
-$character_limit = 500; // Set this variable to whatever limit you want, if set to 0 it's unlimited
-
+$characterLimit = 500; // Set the limit here, set to 0 for unlimited characters
 // If the form was submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check the captcha
     if ($_SESSION["captcha"] != $_POST["captcha"]) {
-        $errorMsg = "Invalid captcha (It's case sensitive)";
+        $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');
+        $errorMsg = "Invalid captcha, it's case sensitive<br><h3>Your Message</h3><p>" . $message . "</p>";
     } else {
         // Get the form data
         $name = $_POST['name'] ? $_POST['name'] : "anon";
-        $message = $_POST['message'];
-        if (!empty($message)) {
-            if ($character_limit > 0 && strlen($message) > $character_limit) {
-                $errorMsg = "Your message exceeds the character limit of " . $character_limit;
-            } else {
-                // Load the existing data
-                $data = array();
-                if (file_exists('data.json') && filesize('data.json') > 0) {
-                    $data = json_decode(file_get_contents('data.json'), true);
+        $message = htmlspecialchars($_POST['message'], ENT_QUOTES, 'UTF-8');;
+        // Check the character limit
+        if ($characterLimit > 0 && strlen($message) > $characterLimit) {
+            $errorMsg = "Message exceeds the character limit of $characterLimit characters<br><h3>Your Message</h3><p>" . $message . "</p>";
+        } else if (!empty($message)) {
+            // Load the existing data
+            $data = array();
+            if (file_exists('data.json') && filesize('data.json') > 0) {
+                $data = json_decode(file_get_contents('data.json'), true);
+            }
+            // Check if the message already exists
+            $flag = true;
+            foreach ($data as $submission) {
+                if ($submission['name'] == $name && $submission['message'] == $message) {
+                    $flag = false;
+                    break;
                 }
-                // Check if the message already exists
-                $flag = true;
-                foreach ($data as $submission) {
-                    if ($submission['name'] == $name && $submission['message'] == $message) {
-                        $flag = false;
-                        break;
-                    }
-                }
-                // If the message is new, add it to the data
-                if ($flag) {
-                    array_unshift($data, array("name" => $name, "message" => $message, "rname" => "", "rmessage" => ""));
-                    file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT));
-                }
+            }
+            // If the message is new, add it to the data
+            if ($flag) {
+                array_unshift($data, array("name" => $name, "message" => $message, "rname" => "", "rmessage" => ""));
+                file_put_contents('data.json', json_encode($data, JSON_PRETTY_PRINT));
             }
         }
     }
@@ -47,7 +45,7 @@ $data = json_decode($json, true);
     <form action="" method="post">
         <input class="box" type="text" id="name" name="name" placeholder="enter name or leave empty for anon"><br><br>
         <textarea class="box" id="message" name="message" placeholder="enter your message"></textarea><br><br>
-        <img src="captcha.php" alt="captcha"> <input class="box" type="text" name="captcha" placeholder="enter the captcha"><br><br>
+        <img src="/assets/main/pages/captcha.php" alt="captcha"> <input class="box" type="text" name="captcha" placeholder="enter the captcha"><br><br>
         <input class="button" type="submit" name="submit" value="Submit">
     </form>
     <?php if (isset($errorMsg)): ?>
